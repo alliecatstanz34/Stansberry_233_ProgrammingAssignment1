340c88cb5374738c03f68a6194a9c899e4d488fa
alliecatstanz34 <alliecatstanz34@gmail.com>
2020-09-09 16:15:01 -0700

    forgot to ctrl s before last commit

--- Benchmarking.java
+++ Benchmarking.java
@@ -1,5 +1,20 @@
 import java.util.*;
-
+import java.io.*;
 public class Benchmarking {
     LinkedList dataset = new LinkedList();
+
+    public static void main(String[] args) throws FileNotFoundException{
+        File f;
+        if ((args.length == 2)){
+            f = new File(args[0]);
+        }else{
+            Scanner consoleScanner = new Scanner (System.in);
+            System.out.println("What is the name of the file you wish to use? Press ENTER after input: ");
+            f = new File(consoleScanner.next());
+            consoleScanner.close();
+        }
+        Scanner fileScanner  = new Scanner(f);
+
+        fileScanner.close();
+    }
 }
